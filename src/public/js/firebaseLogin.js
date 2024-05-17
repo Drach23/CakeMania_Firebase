38e98b969778e23d153a6008af683f0989eaa92d
Drach23 <drach2003@hotmail.com>
2024-05-16 22:21:30 -0600

    Logica para transportar los datos del usuario que inicio sesion y logica para el logout

--- src/public/js/firebaseLogin.js
+++ src/public/js/firebaseLogin.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-analytics.js";
-import { getFirestore, doc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
-import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+import { getFirestore, doc, getDoc } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-firestore.js";
+import { getAuth, signInWithEmailAndPassword, signOut } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDNip1hN7oluDfRsVcyZ04-RWMmyQQkdvM",
   authDomain: "cakemania-908db.firebaseapp.com",
   projectId: "cakemania-908db",
-  storageBucket: "cakemania-908db.appspot.com",
+  storageBucket: "cakemania-908db",
   messagingSenderId: "688269458179",
   appId: "1:688269458179:web:6d59d5ef3ad2ce087d4736",
   measurementId: "G-38F4RMDQF4"
@@ -22,59 +22,91 @@ const auth = getAuth(); // Obtiene el objeto de autenticación
 const inputEmail = document.getElementById("inputEmail");
 const inputPassword = document.getElementById("inputPassword");
 
-async function userLogin(){
-    const email = inputEmail.value;
-    const password = inputPassword.value;
-  
-    try {
-      const userCredential = await signInWithEmailAndPassword(auth, email, password);
-      // Si el inicio de sesión es exitoso
-      const user = userCredential.user;
-      
-      // Obtener los datos del usuario desde Firestore
-      const userDocRef = doc(db, "users", user.uid);
-      const userDocSnap = await getDoc(userDocRef);
+async function userLogin() {
+  const email = inputEmail.value;
+  const password = inputPassword.value;
 
-      if (userDocSnap.exists()) {
-          const userData = userDocSnap.data();
-          // Guardar los datos del usuario en la base de datos local o en el estado de la aplicación
-          // Por ejemplo, podrías guardarlos en el localStorage
-          localStorage.setItem("userData", JSON.stringify(userData));
-          
-          alert("Inicio de sesión exitoso");
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    const user = userCredential.user;
 
-          // Obtener los datos del usuario almacenados en el localStorage
-const userDataString = localStorage.getItem("userData");
+    const userDocRef = doc(db, "users", user.uid);
+    const userDocSnap = await getDoc(userDocRef);
 
-// Verificar si hay datos de usuario en el localStorage
-if (userDataString) {
-  // Si hay datos, convertir la cadena JSON a un objeto JavaScript
-  const userData = JSON.parse(userDataString);
-  
-  // Ahora puedes acceder a los datos del usuario individualmente
-  const email = userData.email;
-  const firstName = userData.firstname;
-  const lastName = userData.lastname;
-  const phone = userData.phone;
-  const username = userData.username;
-  
-  // Hacer lo que necesites con los datos del usuario
-  console.log(email, firstName, lastName, phone, username);
-} else {
-  console.log("No se encontraron datos de usuario en el localStorage");
-}
+    if (userDocSnap.exists()) {
+      const userData = userDocSnap.data();
+      localStorage.setItem("userData", JSON.stringify(userData));
 
-      } else {
-          console.error("No se encontraron datos de usuario en la base de datos");
-      }
-    } catch (error) {
-      console.error("Error al iniciar sesión:", error);
-      alert("Error al iniciar sesión:", error.message);
+      alert("Inicio de sesión exitoso");
+      window.location.href = "/"; // Redirecciona al inicio
+
+    } else {
+      console.error("No se encontraron datos de usuario en la base de datos");
     }
+  } catch (error) {
+    console.error("Error al iniciar sesión:", error);
+    alert("Error al iniciar sesión: " + error.message);
+  }
 }
 
 const buttonLogin = document.querySelector(".login__button");
-buttonLogin.addEventListener("click", (event) => {
-  event.preventDefault(); // Evita la recarga de la página
-  userLogin(); // Llama a la función de inicio de sesión
+if (buttonLogin) {
+  buttonLogin.addEventListener("click", (event) => {
+    event.preventDefault(); // Evita la recarga de la página
+    userLogin(); // Llama a la función de inicio de sesión
+  });
+}
+
+function getUserDataFromLocalStorage() {
+  const userDataString = localStorage.getItem("userData");
+  return userDataString ? JSON.parse(userDataString) : null;
+}
+
+function updateUserInfo() {
+  const userInfoDiv = document.getElementById("user-info");
+  const userData = getUserDataFromLocalStorage();
+
+  if (userData) {
+    userInfoDiv.innerHTML = 
+    `<div class="dropdown">
+    <p class="login_welcomeMessage">Bienvenido, ${userData.username}</p>
+    <div class="dropdown-content">
+      <a href="/profile">Configuración de perfil</a>
+      <a href="#" id="logoutButton">Cerrar sesión</a>
+    </div>
+  </div>`;
+  } else {
+    userInfoDiv.innerHTML = `<a class="btn" href="/login" class="btn"><button>Iniciar sesión</button></a>`;
+  }
+}
+// Función para cerrar sesión
+function userLogout() {
+  const auth = getAuth();
+  signOut(auth).then(() => {
+    // Borrar los datos del usuario del localStorage
+    localStorage.removeItem("userData");
+
+    // Redireccionar al usuario a la página de inicio de sesión o a otra página
+    window.location.href = "/login";
+  }).catch((error) => {
+    console.error("Error al cerrar sesión:", error);
+    alert("Error al cerrar sesión: " + error.message);
+  });
+}
+
+// Adjuntar evento de clic al botón de logout después de que se cargue el DOM
+const userInfoDiv = document.getElementById("user-info");
+userInfoDiv.addEventListener("click", (event) => {
+  const logoutButton = event.target.closest("#logoutButton");
+  if (logoutButton) {
+    event.preventDefault();
+    userLogout();
+  }
+});
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM fully loaded and parsed");
+  updateUserInfo();
 });
