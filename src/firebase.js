e7a371c6bdb012868bc27cb297e766cf7bfee315
Drach <drach2003@hotmail.com>
2025-02-10 09:48:05 -0600

    Cambios para realizar un deploy

--- src/firebase.js
+++ src/firebase.js
@@ -4,11 +4,11 @@ const {getFirestore} = require('firebase-admin/firestore')//obtiene el modulo de
 const {initializeApp, applicationDefault} = require('firebase-admin/app')//obtiene el modulo firebase-admin desde los paquetes
 
 
-initializeApp({//Obtiene las credenciales de administrador 
-    credential: applicationDefault(),
-    storageBucket: 'gs://cakemania-908db.appspot.com'
-})
+const adminConfig = process.env.GOOGLE_APPLICATION_CREDENTIALS
+  ? { credential: applicationDefault(), storageBucket: 'gs://cakemania-908db.appspot.com' }
+  : { credential: cert(require('./config/serviceAccountKey.json')), storageBucket: 'gs://cakemania-908db.appspot.com' };
 
+initializeApp(adminConfig);
 const db = getFirestore()//creamos la constante que tiene acceso a la base de datos
 
 module.exports = {
